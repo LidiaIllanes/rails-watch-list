7d43cb7a5b7d09096496a8cab4d9bf8e3124b44f
lidia illanes <l.illanes.roel@gmail.com>
2022-02-17 16:24:32 +0100

    lists

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+    def new
+      @list = List.find(params[:id])
+      @bookmark = Bookmark.new
+    end
+
+    def create
+      @list = List.find(params[:id])
+      @bookmark = Bookmark.new(bookmark_params)
+
+       if @bookmark.save
+        redirect_to @list, notice: "created"
+       else
+        render :new
+       end
+    end
+
+    def destroy
+      @bookmark = Bookmark.find(params[:id])
+      @bookmark.destroy
+      redirect_to @list
+    end
+    
+    private
+
+    def bookmark_params
+      params.require(:bookmark).permit(:comment, :movie_id)
+    end
+end
